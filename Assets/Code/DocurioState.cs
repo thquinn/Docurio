2021ebbfd4aed047de769684b1ac32c75541b990
Tom Quinn <thomashquinn@gmail.com>
2021-10-02 00:22:40 -0400

    Initial commit

---
+++ Assets/Code/DocurioState.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Code {
+    public class DocurioState {
+        static int MAX_HEIGHT = 3;
+
+        public DocurioEntity[,,] board;
+        public int playerToMove = 0;
+
+        public DocurioState(int size) {
+            bool kingPlaced = false;
+            board = new DocurioEntity[size, size, MAX_HEIGHT];
+            for (int x = 0; x < size; x++) {
+                for (int y = 0; y < size; y++) {
+                    if (UnityEngine.Random.value < .5f) {
+                        board[x, y, 0] = DocurioEntity.Block;
+                        if (!kingPlaced) {
+                            board[x, y, 1] = DocurioEntity.King;
+                            kingPlaced = true;
+                        } else if (UnityEngine.Random.value < .25f) {
+                            board[x, y, 1] = DocurioEntity.Block;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public enum DocurioEntity {
+        Empty, Block, King
+    }
+}
---
+++ Assets/Scripts/GameBoardScript.cs
@@ -0,0 +1,46 @@
+﻿using Assets.Code;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoardScript : MonoBehaviour
+{
+    public GameObject tilePrefab, blockPrefab, kingPrefab;
+    float entityScale;
+
+    DocurioState state;
+    GameObject[,] tileObjects;
+    GameObject[,,] entityObjects;
+
+    void Start() {
+        int size = 7;
+        state = new DocurioState(size);
+        tileObjects = new GameObject[size, size];
+        entityObjects = new GameObject[size, size, state.board.GetLength(2)];
+        float mid = (size - 1) / 2;
+        entityScale = blockPrefab.transform.localScale.y;
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                GameObject tile = Instantiate(tilePrefab, transform);
+                tile.transform.localPosition = new Vector3(x - mid, 0, y - mid);
+                tileObjects[x, y] = tile;
+                for (int z = 0; z < state.board.GetLength(2); z++) {
+                    GameObject entity = null;
+                    if (state.board[x, y, z] == DocurioEntity.Block) {
+                        entity = Instantiate(blockPrefab, transform);
+                    } else if (state.board[x, y, z] == DocurioEntity.King) {
+                        entity = Instantiate(kingPrefab, transform);
+                    } else {
+                        break;
+                    }
+                    entity.transform.localPosition = new Vector3(x - mid, entityScale * z, y - mid);
+                    entityObjects[x, y, z] = entity;
+                }
+            }
+        }
+    }
+
+    void Update() {
+        
+    }
+}
