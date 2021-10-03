e2bf8cc4cbf2d3480ee5a7a2b49e2765257cfd09
Tom Quinn <thomashquinn@gmail.com>
2021-10-03 18:42:11 -0400

    icons, fade

---
+++ Assets/Scripts/FadeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeScript : MonoBehaviour
+{
+    public CanvasGroup canvasGroup;
+
+    void Start() {
+        if (!Application.isEditor) {
+            canvasGroup.alpha = 1;
+        }
+    }
+    void Update()
+    {
+        canvasGroup.alpha -= .01f;
+        if (canvasGroup.alpha <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/ScribbleScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScribbleScript : MonoBehaviour
+{
+    public Image image;
+    public Sprite otherScribble;
+    Sprite firstScribble;
+    int frame;
+
+    void Start() {
+        firstScribble = image.sprite;
+    }
+    void Update()
+    {
+        if (++frame == 20) {
+            image.sprite = otherScribble;
+        }
+        if (frame == 40) {
+            image.sprite = firstScribble;
+            frame = 0;
+        }
+    }
+}
